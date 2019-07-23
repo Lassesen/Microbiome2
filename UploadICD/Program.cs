20cdc27e4b7a9080eb69efa3e18dfd816958bf3d
Ken Lassesen <Ken@lassesen.con>
2019-07-22 19:49:19 -0700

    Adding Tabular data

--- UploadICD/Program.cs
+++ UploadICD/Program.cs
@@ -10,36 +10,52 @@ namespace UploadICD
     {
         static void Main(string[] args)
         {
-            FileInfo xmlFile = null;
-             var showHelp = args.Length <1;
+            FileInfo indexFile = null;
+            FileInfo tabularFile = null;
+            var showHelp = args.Length <2;
             //Check folder exists
             if (!showHelp)
             {
-                xmlFile= new FileInfo(args[0]);
-                if (!xmlFile.Exists)
+                indexFile= new FileInfo(args[0]);
+                if (!indexFile.Exists)
                 {
-                    Console.Write($"The folder {xmlFile.FullName} does not exist.");
+                    Console.Write($"The index file {indexFile.FullName} does not exist.");
                     showHelp = true;
                 }
+                tabularFile = new FileInfo(args[1]);
+                if (!tabularFile.Exists)
+                {
+                    Console.Write($"The tabular file  {tabularFile.FullName} does not exist.");
+                    showHelp = true;
+                }
+
             }
             if (showHelp)
             {
                 Console.WriteLine(@"
 UploadICD - Uploads ICF taxonomy information
-         UploadICD {PathtoFile}
+         UploadICD {indexFile} {TabularFile}
 File should be downloaded from  https://www.cms.gov/medicare/coding/icd10/2019-icd-10-cm.html 
-and be this file icd10cm_index_2019.xml or equivalent
+and be this file icd10cm_index_2019.xml  icd10cm_tabular_2019.xml
+Ore later versions
 ");
 
                 return;
             }
 
             //We need to remove encoding to get the insert working
-            var xmlList = File.ReadAllLines(xmlFile.FullName);                ;
+            var xmlList = File.ReadAllLines(indexFile.FullName);                ;
             var xml = new StringBuilder();
             for (var i = 1; i  < xmlList.Length ;i++)
             {
                 xml.Append(xmlList[i]);
+            }
+
+            xmlList = File.ReadAllLines(tabularFile.FullName); ;
+            var txml = new StringBuilder();
+            for (var i = 1; i < xmlList.Length; i++)
+            {
+                txml.Append(xmlList[i]);
             }
 
             var connectionString = File.ReadAllText("DBString.txt");
@@ -50,7 +66,8 @@ and be this file icd10cm_index_2019.xml or equivalent
                 {
                     cmd.CommandType = CommandType.StoredProcedure;
                     cmd.CommandTimeout = 900;
-                    cmd.Parameters.AddWithValue("@xml", xml.ToString());
+                    cmd.Parameters.AddWithValue("@IndexXml", xml.ToString());
+                    cmd.Parameters.AddWithValue("@TabularXml", txml.ToString());
                     cmd.ExecuteNonQuery();
                 }
             }
