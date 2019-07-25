45fdf5e4e9fa2ff2ec1df9c4652f9c9474cbacbe
Ken Lassesen <Ken@lassesen.con>
2019-07-24 17:45:58 -0700

    DB Version of citations

---
+++ DBV2/dbo/Stored Procedures/UploadPubMed.sql
@@ -0,0 +1,47 @@
+﻿CREATE Proc UploadPubMed
+@Pmid int,
+@Title Nvarchar(255)
+           ,@CitationJson   nvarchar(max)=null
+		   ,@RawText nvarchar(max)=null
+           ,@Summary nvarchar(max)=null
+            ,@PmcId  varchar(14)=null
+           ,@Doi varchar(60)=null
+           ,@Pii varchar(50) =null
+AS
+If NOT EXISTS(Select 1 From Citation where Pmid=@Pmid OR @Title=Title)
+BEGIN
+INSERT INTO [dbo].[Citation]
+           ([Title]
+           ,[CitationJson]
+           ,[RawText]
+            ,[Summary]
+           ,[PmId]
+           ,[PmcId]
+           ,[Doi]
+           ,[Pii] )
+     VALUES
+           (@Title
+           ,@CitationJson
+           ,@RawText
+           ,@Summary 
+           ,@PmId 
+           ,@PmcId 
+           ,@Doi
+           ,@Pii
+           )
+		   SELECT @@IDENTITY
+End
+ELSE
+BEGIN
+UPDATE [dbo].[Citation]
+   SET [Title] = @Title
+      ,[CitationJson] = @CitationJson
+      ,[RawText] = @RawText
+      ,[Summary] = @Summary
+      ,[PmcId] = @PmcId
+      ,[Doi] = @Doi
+      ,[Pii] =  @Pii 
+ 
+ WHERE @Pmid=PmId OR @Title=Title
+ Select pmid From Citation where Pmid=@Pmid OR @Title=Title
+END
---
+++ DBV2/dbo/Tables/Citation.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[Citation] (
+    [CitationId]          INT             IDENTITY (1, 1) NOT NULL,
+    [Title]               NVARCHAR (255)  NOT NULL,
+    [CitationJson]        NVARCHAR (MAX)  NULL,
+    [RawText]             NVARCHAR (MAX)  NULL,
+    [RawDocument]         VARBINARY (MAX) NULL,
+    [RawDocumentMimeType] VARCHAR (60)    NULL,
+    [Summary]             NVARCHAR (MAX)  NULL,
+    [PmId]                INT             NULL,
+    [PmcId]               VARCHAR (14)    NULL,
+    [Doi]                 VARCHAR (60)    NULL,
+    [Pii]                 VARCHAR (50)    NULL,
+    [Url]                 VARCHAR (255)   NULL,
+    CONSTRAINT [PK_Citation] PRIMARY KEY CLUSTERED ([CitationId] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Citation]
+    ON [dbo].[Citation]([Title] ASC);
+
