c240a9f848533163572071b8248a8d2e15c5ee24
Ken Lassesen <Ken@lassesen.con>
2019-07-26 21:17:26 -0700

    PDF Example

---
+++ uploadXenoGene/Program.cs
@@ -0,0 +1,68 @@
+﻿ 
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace TaxonNameUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo TaxonNameUpload = null;
+            var showHelp = args.Length < 1;
+            //Check folder exists
+            if (!showHelp)
+            {
+                TaxonNameUpload = new FileInfo(args[0]);
+                if (!TaxonNameUpload.Exists)
+                {
+                    Console.Write($"The file {TaxonNameUpload.FullName} does not exist.");
+                    showHelp = true;
+                }
+
+            }
+            if (showHelp)
+            {
+                Console.WriteLine(@"
+UploadXenoGene - Uploads a PDF file from XenoGene Spain
+         UploadXenoGene {filepath}
+");
+
+                return;
+            }
+
+             var parsedFile = new XenoGene(TaxonNameUpload);          
+            var connectionString = File.ReadAllText("DBString.txt");
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("uploadTaxNameRank", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = 900;
+                    SqlParameter hParameter = new SqlParameter();
+                    hParameter.ParameterName = "@Data";
+                    hParameter.SqlDbType = SqlDbType.Structured;
+                    hParameter.Value = parsedFile.AsDataTable;
+                    cmd.Parameters.Add(hParameter);
+                    cmd.Parameters.AddWithValue("@LabName",  parsedFile.Lab);
+                    cmd.Parameters.AddWithValue("@SampleId", parsedFile.SampleId);
+                    cmd.Parameters.AddWithValue("@LabTestName", parsedFile.LabTest);
+                    cmd.Parameters.AddWithValue("ownerEmail", "Ken@Lassesen.com");
+                    DateTime sampleDate = DateTime.UtcNow; // replace as appropriate                   
+                    cmd.Parameters.AddWithValue("SampleDate", sampleDate);                 
+                    var dataSet = new DataSet(parsedFile.LabTest);
+                    var sqladap = new SqlDataAdapter(cmd);
+                    sqladap.Fill(dataSet);
+                    if (dataSet.Tables[0].Rows.Count > 0)
+                    {
+                        Console.WriteLine($"Could not locate all taxon names in   {parsedFile.LabTest}");
+                        dataSet.WriteXml($"Missing_{parsedFile.SampleId}.xml");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ uploadXenoGene/XenoGene.cs
@@ -0,0 +1,158 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Data;
+using System.IO;
+using System.Text;
+
+namespace TaxonNameUpload
+{
+    public class XenoGene
+    {
+        DataTable _dataTable = new DataTable();
+        public XenoGene(FileInfo file)
+        {
+            _dataTable.Columns.Add("tax_rank", typeof(string));
+            _dataTable.Columns.Add("tax_name", typeof(string));
+            _dataTable.Columns.Add("BaseOneMillion", typeof(double));
+
+            char[] lineSep = { '\r','\n'   };
+            char[] sep = { ' '  };
+            var txt = ReadPdfFile(file.FullName);
+            var lines = txt.Split(lineSep, StringSplitOptions.RemoveEmptyEntries);
+            string appliesToNextLine = "";
+            int lineNo = 0;
+            double amount = 0.0;
+            foreach (var line in lines)
+            {
+                var skip = false;
+                string rank = "";
+                string name = "";
+                if (line.IndexOf("ORDEN",StringComparison.OrdinalIgnoreCase)==0) skip = true;
+                if (line.IndexOf("INFO", StringComparison.OrdinalIgnoreCase) == 0) skip = true;
+                if (line.IndexOf("REPORT", StringComparison.OrdinalIgnoreCase) == 0) skip = true;
+                if (line.IndexOf("Xeno", StringComparison.OrdinalIgnoreCase) == 0) skip = true;
+
+                if (line.IndexOf("Inscrita", StringComparison.OrdinalIgnoreCase) == 0) skip = true;
+                if (line.IndexOf("Pág", StringComparison.OrdinalIgnoreCase) == 0) skip = true;
+                if(!skip)
+                {
+                    var firstSpace = line.IndexOf(" ");
+                    var firstComma = line.IndexOf(",");
+                    var lineNoTest = firstSpace < 0 ? "na": line.Substring(0, firstSpace);
+                    if(int.TryParse(lineNoTest,out lineNo))
+                    {
+                        //If missing no number
+                        if(firstComma < 0)
+                        {
+                            name = line;
+                        }
+                        else
+                        {
+                            var parts = line.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+                            var lastItem = parts[parts.Length - 1];
+                            if (lastItem.Contains(",")) //european number
+                            {
+                                lastItem = lastItem.Replace(",", ".");
+                                if (double.TryParse(lastItem, out amount))
+                                {
+                                    amount = amount * 10000.0; //permission
+                                    switch (parts[2])
+                                    {
+                                        case "G":rank = "genus";break;
+                                        case "G1": rank = "genus"; break;
+                                        case "G2": rank = "genus"; break;
+                                        case "C": rank = "class"; break;
+                                        case "C1": rank = "class"; break;
+                                        case "C2": rank = "class"; break;
+                                        case "F": rank = "family"; break;
+                                        case "S": rank = "species"; break;
+                                        case "S1": rank = "strain"; break;
+                                        case "S2": rank = "strain"; break;
+                                        case "S3": rank = "strain"; break;
+                                        case "O": rank = "Order"; break;
+                                        case "F1": rank = "no rank"; break;
+                                        case "F2": rank = "no rank"; break;
+                                        case "F3": rank = "no rank"; break;
+                                        case "O1": rank = "no rank"; break;
+                                        case "O2": rank = "no rank"; break;
+                                        case "O3": rank = "no rank"; break;
+                                        case "P1": rank = "no rank"; break;
+                                        case "D1": rank = "skip"; break;
+                                        case "D2": rank = "skip"; break;
+                                        case "D3": rank = "skip"; break;
+                                        case "U": rank = "skip"; break;
+                                        case "R": rank = "skip"; break;
+                                        case "R1": rank = "skip"; break;
+                                        case "P": rank = "phylum"; break;
+                                        default:
+                                            rank = "skip";
+                                            Console.WriteLine($"Unknown {parts[2]}");
+                                            break;
+                                    }
+                                    if (parts.Length > 4)
+                                    {
+                                        var sb = new StringBuilder(parts[3]);
+                                        for (var p = 4; p < parts.Length - 1; p++)
+                                        {
+                                            sb.Append($" {parts[p]}");
+                                        }
+                                        name = sb.ToString();
+                                    }
+                                    
+                                    if(!rank.Equals("skip",StringComparison.OrdinalIgnoreCase)  && amount > 0.0 && ! string.IsNullOrWhiteSpace(name))
+                                        {
+                                        Console.WriteLine(name);
+                                        var row = _dataTable.NewRow();
+                                        row["tax_rank"] = rank;
+                                        row["tax_name"] = name;
+                                        row["BaseOneMillion"] = amount;
+                                        _dataTable.Rows.Add(row);
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        appliesToNextLine = line;
+                    }
+
+
+                }
+                
+
+
+
+            }
+            SampleId = file.Name;
+         
+        }
+        public DataTable AsDataTable
+        { get { return _dataTable; } }
+        public string LabTest
+        { get { return "American Gut"; } }
+        public string Lab
+        {            get { return "American Gut"; }        }
+        public string SampleId
+        { get; set; }
+        private string ReadPdfFile(object Filename )
+        {
+            PdfReader reader2 = new PdfReader((string)Filename);
+            string strText = string.Empty;
+
+            for (int page = 1; page <= reader2.NumberOfPages; page++)
+            {
+                ITextExtractionStrategy its = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+                PdfReader reader = new PdfReader((string)Filename);
+                String s = PdfTextExtractor.GetTextFromPage(reader, page, its);
+
+                s = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(s)));
+                strText = strText + s;
+                reader.Close();
+            }
+            return strText;
+        }
+    }
+}
