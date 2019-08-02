68729d5c0a6ec62b7477fa75d23e53cc2183fb53
Ken Lassesen <Ken@lassesen.con>
2019-08-02 06:21:23 -0700

    Dedup code

---
+++ DBV2/dbo/Tables/CategoryAssociations.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CategoryAssociations] (
+    [CategoriIdA] INT NOT NULL,
+    [CategoryIdB] INT NOT NULL,
+    CONSTRAINT [PK_CategoryAssociations] PRIMARY KEY CLUSTERED ([CategoriIdA] ASC, [CategoryIdB] ASC),
+    CONSTRAINT [FK_CategoryAssociations_CategoryReference] FOREIGN KEY ([CategoriIdA]) REFERENCES [dbo].[CategoryReference] ([CategoryId]),
+    CONSTRAINT [FK_CategoryAssociations_CategoryReference1] FOREIGN KEY ([CategoryIdB]) REFERENCES [dbo].[CategoryReference] ([CategoryId])
+);
+
---
+++ DBV2/dbo/Tables/CategoryContinuousAssociation.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CategoryContinuousAssociation] (
+    [ContinuousId] INT NOT NULL,
+    [CategoryId]   INT NOT NULL,
+    CONSTRAINT [PK_CategoryContinuousAssociation] PRIMARY KEY CLUSTERED ([ContinuousId] ASC, [CategoryId] ASC),
+    CONSTRAINT [FK_CategoryContinuousAssociation_CategoryReference] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[CategoryReference] ([CategoryId]),
+    CONSTRAINT [FK_CategoryContinuousAssociation_ContinuousReference] FOREIGN KEY ([ContinuousId]) REFERENCES [dbo].[ContinuousReference] ([ContinuousId])
+);
+
